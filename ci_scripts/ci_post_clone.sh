56b8238bfe622fc0756dfd1468160acbacc43c0f
Taylor Lineman <git@actuallytaylor.com>
2026-06-25 19:09:22 -0400

    New method for approving macros that doesn’t blindly trust everything.
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+// Idea from: cgontijo @ https://stackoverflow.com/a/78572430
+// We use build tool plugins & swift macros which both require plugin fingerprint validation. Xcode cloud is not able to approve these automatically. To get around this, we copy macros.json and plugins.json from the swiftpm security directory into this project. This should be done every time a new macro or plugin is added into the project.
+
+// Create the swiftpm security directory.
+mkdir -p ~/Library/org.swift.swiftpm/security/
+
+// Trust the macros we have manually trusted.
+cp macros.json ~/Library/org.swift.swiftpm/security/
+
+// Copy the plugins we have manually trusted.
+cp plugins.json ~/Library/org.swift.swiftpm/security/
