0d2f59ae7ce82463ba4265f64a447778369a6a70
Taylor Lineman <git@actuallytaylor.com>
2026-07-16 15:00:55 -0400

    Passable background on the file window
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

--- Minna/Views/File Viewer/FileViewer.swift
+++ Minna/Views/File Viewer/FileViewer.swift
@@ -10,17 +10,28 @@ import DatabaseSchema
 import SwiftData
 import QuickLookUI
 import LookAtMe
+import SFSafeSymbols
 
 struct FileViewer: View {
+    @Environment(\.openURL) var openURL
+    
     let file: File
     @State var previewURL: URL?
     
     var body: some View {
-        HStack {
-            content
-            
-            sidebar
+        ZStack {
+            // HACK: This does the job of container background without making the entire toolbar a solid color.
+            file.color.background.ignoresSafeArea() // Used to fill the background color.
+            ScrollView { } // A scroll view just existing toggles the toolbar to be transparent.
+
+            HStack {
+                content
+                    .ignoresSafeArea(.container, edges: .top)
+                
+                sidebar
+            }
         }
+        .navigationTitle(file.title)
         .onAppear {
             do {
                 let scopedURL = try file.securityScopedURL()
@@ -33,6 +44,20 @@ struct FileViewer: View {
         }
         .onDisappear {
             try? file.securityScopedURL().stopAccessingSecurityScopedResource()
+        }
+        .toolbar {
+            ToolbarItem {
+                Button {
+                    do {
+                        try file.openOriginal(openURL: openURL)
+                    } catch {
+                        print("Failed to open file: \(error)")
+                    }
+                } label: {
+                    Label("Open Original", systemSymbol: .arrowUpForward)
+                }
+                .labelStyle(.titleAndIcon)
+            }
         }
     }
     
