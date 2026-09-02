232d22b3112f96b7809552fef0528058b08a684d
Taylor Lineman <git@actuallytaylor.com>
2026-09-02 17:11:28 -0400

    Added post clone step for setting xcconfig

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+set -euo pipefail
+
 # Idea from: cgontijo @ https://stackoverflow.com/a/78572430
 # We use build tool plugins & swift macros which both require plugin fingerprint validation. Xcode cloud is not able to approve these automatically. To get around this, we copy macros.json and plugins.json from the swiftpm security directory into this project. This should be done every time a new macro or plugin is added into the project.
 
@@ -11,3 +13,11 @@ cp macros.json ~/Library/org.swift.swiftpm/security/
 
 # Copy the plugins we have manually trusted.
 cp plugins.json ~/Library/org.swift.swiftpm/security/
+
+
+# Write an XCConfig with variables from Xcode clouds' environment
+cp "../Config.local.example.xcconfig" "../Config.local.xcconfig"
+
+sed -i '' -E "s/^DEVELOPMENT_TEAM[[:space:]]*=.*/DEVELOPMENT_TEAM = ${DEVELOPMENT_TEAM:-}/" ../Config.local.xcconfig
+sed -i '' -E "s/^SENTRY_DSN[[:space:]]*=.*/SENTRY_DSN = ${SENTRY_DSN:-}/" ../Config.local.xcconfig
+sed -i '' -E "s/^TELEMETRY_DECK_ID[[:space:]]*=.*/TELEMETRY_DECK_ID = ${TELEMETRY_DECK_ID:-}/" ../Config.local.xcconfig
